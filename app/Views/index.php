772d0a036c5914368169152ddc571482c70f2d09
Riyan Fitriadi <riyanfitriadi@gmail.com>
2021-06-21 17:33:31 +0700

    Initial Commit

---
+++ app/Views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SMKN 1 BRN</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+</head>
+
+<body>
+    <?php include 'menu.php'; ?>
+    <div class="container"></div>
+</body>
+
+</html>
---
+++ app/Views/menu.php
@@ -0,0 +1,35 @@
+<header>
+    <nav>
+        <ul class="nav_links">
+            <li><a href=""><i class="fas fa-home"></i>Beranda</a></li>
+            <li><a href=""><i class="fas fa-book"></i>Profil</a>
+                <div class="sub-menu">
+                    <ul>
+                        <li><a href="#">Sejarah</a></li>
+                        <li><a href="#">Visi dan Misi</a></li>
+                        <li><a href="#">Kepalaa Sekolah</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href=""><i class="fas fa-newspaper"></i>Berita</a></li>
+            <li><a href=""><i class="fas fa-graduation-cap"></i>Program keahlian</a>
+                <div class="sub-menu">
+                    <ul>
+                        <li><a href="#">Teknik Kendaraan Ringan Otomotif</a></li>
+                        <li><a href="#">Teknik Komputer dan Jaringan</a></li>
+                        <li><a href="#">Teknik Pengelasan</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href=""><i class="fas fa-clone"></i>Kegiatan</a>
+                <div class="sub-menu">
+                    <ul>
+                        <li><a href="#">PPDB</a></li>
+                        <li><a href="#">Ekstrakurikuler</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href=""><i class="fas fa-images"></i>Galeri</a></li>
+        </ul>
+    </nav>
+</header>
